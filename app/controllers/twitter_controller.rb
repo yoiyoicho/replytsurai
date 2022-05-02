00429c576ca56e83abfdd9afd1b1a4d249aa19a4
yoshino <minakoyoshino3@gmail.com>
2022-05-03 00:28:00 +0900

    twitter show view modify

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -60,6 +60,7 @@ class TwitterController < ApplicationController
     }
     request = Typhoeus::Request.new(url, options)
     response = request.run
+    byebug
     if response.code == 200
       JSON.parse(response.body)
     else
