ff6ae3cf210fe4cbbcf0c5184f7faad51ebb841e
yoshino <minakoyoshino3@gmail.com>
2022-05-19 23:19:26 +0900

    yahoo_controller modify

--- app/controllers/yahoo_controller.rb
+++ app/controllers/yahoo_controller.rb
@@ -21,10 +21,9 @@ class YahooController < ApplicationController
   def get_reply(text)
     yahoo_url = 'https://jlp.yahooapis.jp/NLUService/V1/analyze'
     appid = ENV['YAHOO_APP_ID']
-    method = 'SAY'
 
     escape_text =  URI.escape(text)
-    url = yahoo_url + '?appid=' + appid + '&intext=' + escape_text + '&sample0=' + method + ',' + escape_text
+    url = yahoo_url + '?appid=' + appid + '&intext=' + escape_text
     uri = URI.parse(url)
     response = Net::HTTP.get_response(uri)
 
